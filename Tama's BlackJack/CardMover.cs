9c2a5ab3ce03053117b8ae5a86b7ac62b8383b2a
20205073 <20205073@g.aut.ac.jp>
2022-06-22 14:33:35 +0900

    不明な関数を削除

--- Tama's BlackJack/CardMover.cs
+++ Tama's BlackJack/CardMover.cs
@@ -8,9 +8,5 @@ namespace Tama_s_BlackJack
 {
     public static class CardMover
     {
-        public static void MoveCard(System.Windows.Forms.PictureBox card, System.Windows.Forms.PictureBox deck, int time)
-        {
-            
-        }
     }
 }
