65f64ae38fa7ef9f6d5009cb964abbc291b5802c
20205073 <20205073@g.aut.ac.jp>
2022-10-19 10:05:43 +0900

    ・期待値計算クラスを更新

--- Tama's BlackJack/BlackjackExpectedValueCalculator.cs
+++ Tama's BlackJack/BlackjackExpectedValueCalculator.cs
@@ -9,15 +9,45 @@ namespace Tama_s_BlackJack
     class BlackjackExpectedValueCalculator
     {
         private CardManager nowCardDeck;
+        private float _playerWinPercentage;
+        private float _playerDrawPercentage;
+        private float _playerLosePercentage;
+        private bool cardDeckReset;
 
         public BlackjackExpectedValueCalculator()
         {
             nowCardDeck = null;
+            _playerWinPercentage = 0.0f;
+            _playerDrawPercentage = 0.0f;
+            _playerLosePercentage = 0.0f;
+            cardDeckReset = false;
         }
 
         public void SetNowCardDeck(CardManager cm)
         {
             nowCardDeck = cm;
+            _playerWinPercentage = 0.0f;
+            _playerDrawPercentage = 0.0f;
+            _playerLosePercentage = 0.0f;
+            cardDeckReset = false;
+        }
+
+        private void CalcPlayerWDLPercentage(int dealerOpenCard, int playerTotalCard)
+        {
+            if (cardDeckReset) throw new CardDeckNotResetException();
+
+
+
+            cardDeckReset = true;
+        }
+
+        public class CardDeckNotResetException : Exception
+        {
+            public CardDeckNotResetException()
+                : base("カードデッキがリセットされていません")
+            {
+
+            }
         }
     }
 }
