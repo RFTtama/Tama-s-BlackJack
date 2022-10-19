12af10576416e580d27ae763322c039005d8dc73
20205073 <20205073@g.aut.ac.jp>
2022-10-19 09:39:12 +0900

    ・期待値計算クラスを追加

---
+++ Tama's BlackJack/BlackjackExpectedValueCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tama_s_BlackJack
+{
+    class BlackjackExpectedValueCalculator
+    {
+        private CardManager nowCardDeck;
+
+        public BlackjackExpectedValueCalculator()
+        {
+            nowCardDeck = null;
+        }
+
+        public void SetNowCardDeck(CardManager cm)
+        {
+            nowCardDeck = cm;
+        }
+    }
+}
